ca81976c5867d0535e4fa25d8777eed9b54acd9c
gdj <gdeanda@inmegen.edu.mx>
2025-07-21 23:13:48 -0600

    added working vignette and zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+# zzz.R
+
+utils::globalVariables(
+  names = c(
+    "x", "y", "type", "meta_id", "id", "kegg_id", "name",
+    "hgnc_symbol", "subtype", "edges", "member_id", "to",
+    "from", "type_from", "type_to", "kegg_ids", "kegg_id_from", "kegg_id_to"
+  )
+)
